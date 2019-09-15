55145b638cc9e7ee8d4a592327064696b499aa99
Garrett Lender <garrett.lender@gmail.com>
2019-09-15 14:56:55 -0400

    Final update to temperature sensor.

--- Module2/Project2_Temperature.ino
+++ Module2/Project2_Temperature.ino
@@ -1,15 +1,26 @@
+// library to handle time-based interrupts 
 #include <TimerOne.h>
 
+// the reported temperature
 float temperature = 0.0;
+
+// the average temperature from the sensor
 float averageTemperature = 0.0;
+
+// reading output the sensor
 int voltage = 0;
 
 unsigned long time;
+
+// used to exit the program on user input
 bool done = false;
 
 void readTemperature() {
+
+  // obtain an average from the sensor so that we have a better reading 
   averageTemperature = 0;
   for (int i = 1; i <= 5; i++){
+    // read off the sensor
     voltage = analogRead(A0);
     averageTemperature =  averageTemperature + ( ( (voltage / 204.8) - 0.5) / 0.01);
     delay(15);
@@ -23,7 +34,7 @@ void readTemperature() {
 void setup() {
   Serial.begin(9600);
   
-  // Show a welcome message as human-readable ASCII text
+  // display a message to the user
   Serial.println("Project 2 Serial Transmit of Temperature");
   Serial.println("This program transmits the time and temp at a periodic rate of around 10s");
   Serial.println("across a Serial bus like USB to the Host. ");
@@ -49,15 +60,13 @@ void setup() {
 
 void handleUserInput() {
    
-  // Used for reading data from the serial monitor
+  // used for reading data from the serial monitor
   char input;
  
-  // Check to see if ! is available to be read
+  // check to see if the user typed anything
   if (Serial.available()) {     
    
-    // Read the character
-    // Serial.read() returns the first (oldest) character in the buffer 
-    // and removes that byte of data from the buffer
+    // read the character
     input = Serial.read();    
  
     // program should be stopped if the user types !, Ctrl-C, Ctrl-Z
@@ -71,23 +80,26 @@ void handleUserInput() {
 
 void displayTimeAndTemperature() {
    
-  // Capture the time and covert to seconds
+  // capture the time and covert to seconds
   time = millis() / 1000;
  
   // display time, temp in Fahrenheit
   Serial.print(time); 
   Serial.print(" , ");
+  // Celsius to Fahrenheit
   Serial.println((temperature * 9 / 5) + 32);   
 }
 
 void loop() {
 
+  // upon startup, we should wait 30 seconds to allow for the temperature sensor to normalize and adjust
   delay(1000 * 30);
 
   while (!done) {
-  
+    // display time/temp
     displayTimeAndTemperature();
 
+    // determine if we should exit 
     handleUserInput();
 
     // delay 10 seconds
@@ -96,4 +108,3 @@ void loop() {
 
   exit(0);
 }
-
